41659c790d5c7a4e8f4de12d7320a820a4a449e7
Kevin Chen <kevinchen8223@gmail.com>
2014-02-25 01:21:23 -0800

    Added bg image, added shadowradius and button/labels

--- CrystalBall/src/main/java/com/example/crystalball/MainActivity.java
+++ CrystalBall/src/main/java/com/example/crystalball/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 import android.R.*;
@@ -21,6 +22,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         // Declare our View variables
